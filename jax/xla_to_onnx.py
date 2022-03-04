f59aec36e39012eb8ba4cd61bf0265f2b29465f4
Akira Kawata <akirakawata1@gmail.com>
2022-03-04 22:48:12 +0900

    WIP

--- jax/xla_to_onnx.py
+++ jax/xla_to_onnx.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 
 import numpy as np
@@ -50,7 +51,19 @@ def translate_outputs(tuple_shapes):
 
 # Instruction -> (name, ValueInfo, Node)
 def t_instruction(instruction):
-    assert instruction.opcode in ["parameter", "constant", "add", "tuple"]
+    # XLA: https://www.tensorflow.org/xla/operation_semantics
+    # ONNX: https://github.com/onnx/onnx/blob/main/docs/Operators.md
+    assert instruction.opcode in [
+        "parameter",
+        "constant",
+        "add",
+        "tuple",
+        "maximum",
+        "exponential",
+        "log",
+        "dot",
+        "subtract"
+    ]
     if instruction.opcode == "parameter":
         name = str(instruction.id)
         value = shape_proto_to_value_info_proto(str(instruction.id), instruction.shape)
@@ -62,6 +75,26 @@ def t_instruction(instruction):
         inputs = list(map(lambda x: str(x), instruction.operand_ids))
         node = helper.make_node("Add", inputs, [str(instruction.id)])
         return (str(instruction.id), None, node)
+    elif instruction.opcode == "subtract":
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
+        node = helper.make_node("Sub", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+    elif instruction.opcode == "maximum":
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
+        node = helper.make_node("Max", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+    elif instruction.opcode == "exponential":
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
+        node = helper.make_node("Exp", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+    elif instruction.opcode == "log":
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
+        node = helper.make_node("Log", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+    elif instruction.opcode == "dot":
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
+        node = helper.make_node("Gemm", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
     elif instruction.opcode == "tuple":
         # TODO:
         assert len(instruction.operand_ids) == 1
@@ -70,6 +103,7 @@ def t_instruction(instruction):
         return (str(instruction.id), None, node)
 
 
+# See https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#creating-an-onnx-model-using-helper-functions
 def t_computation(computation, onnx_filename):
     name_value_nodes = list(map(t_instruction, computation.instructions))
     input_values = []
@@ -98,7 +132,6 @@ def t_computation(computation, onnx_filename):
     onnx.save(model_def, onnx_filename)
 
 
-# See https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#creating-an-onnx-model-using-helper-functions
 def hlo_proto_to_onnx(hlo_proto, onnx_filename):
     t_computation(hlo_proto.computations[0], onnx_filename)
 
@@ -124,6 +157,14 @@ def translate_and_run(fn, input_values, test_name):
         f.write(xla.as_hlo_text())
     with open(test_name + "_as_hlo_dot_graph.dot", "w") as f:
         f.write(xla.as_hlo_dot_graph())
+    dot_cmd = [
+        "dot",
+        "-Tps",
+        test_name + "_as_hlo_dot_graph.dot",
+        "-o",
+        test_name + "_as_hlo_dot_graph.ps",
+    ]
+    subprocess.run(dot_cmd)
 
     hlo_proto = xla.as_serialized_hlo_module_proto()
     with open(test_name + ".hlo_proto", "wb") as f:
@@ -160,29 +201,109 @@ def test_add(shape):
     assert np.allclose(output_values, outputs[0])
 
 
-def test_sum():
-    x = np.random.normal(size=(32, 32)).astype(np.float32)
-    # y = jnp.sum(x)
+@pytest.mark.parametrize("shape", [(32, 32), (32, 64)])
+def test_sub(shape):
+    test_name = "sub"
 
-    print(x)
+    input_values = [
+        np.random.normal(size=shape).astype(np.float32),
+        np.random.normal(size=shape).astype(np.float32),
+    ]
+    fn = jnp.subtract
+    output_values = fn(*input_values)
 
-    sum_jit = jit(jnp.sum, inline=True)
-    xla = jax.xla_computation(sum_jit)(x)
-    print(xla.as_hlo_text())
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
 
-    with open("sum_as_hlo_text.txt", "w") as f:
-        f.write(xla.as_hlo_text())
-    with open("sum_as_hlo_dot_graph.dot", "w") as f:
-        f.write(xla.as_hlo_dot_graph())
 
-    hlo_proto = xla.as_serialized_hlo_module_proto()
-    with open("sum.hlo_proto", "wb") as f:
-        f.write(hlo_proto)
+@pytest.mark.parametrize("shape", [(32, 32), (32, 64)])
+def test_max(shape):
+    test_name = "maximum"
 
-    with open("sum.hlo_proto", "rb") as f:
-        add_hlo_proto_data = f.read()
-        add_hlo_proto = hlo_pb2.HloModuleProto()
-        add_hlo_proto.ParseFromString(add_hlo_proto_data)
+    input_values = [
+        np.random.normal(size=shape).astype(np.float32),
+        np.random.normal(size=shape).astype(np.float32),
+    ]
+    fn = jnp.maximum
+    output_values = fn(*input_values)
 
-    with open("sum_hlo_proto.txt", "w") as f:
-        f.write(str(add_hlo_proto))
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
+
+
+@pytest.mark.parametrize(
+    "shapes",
+    [
+        pytest.param(((32, 32), (32, 32))),
+        pytest.param(((1024, 32), (32, 128))),
+        pytest.param(((32,), (32,)), marks=pytest.mark.xfail),
+        pytest.param(((64, 32), (32,)), marks=pytest.mark.xfail),
+    ],
+)
+def test_dot(shapes):
+    test_name = "dot"
+
+    input_values = [
+        np.random.normal(size=shapes[0]).astype(np.float32),
+        np.random.normal(size=shapes[1]).astype(np.float32),
+    ]
+    fn = jnp.dot
+    output_values = fn(*input_values)
+
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
+
+
+@pytest.mark.parametrize("shape", [(32, 32), (32, 64)])
+def test_exp(shape):
+    test_name = "exp"
+
+    input_values = [
+        np.random.normal(size=shape).astype(np.float32),
+    ]
+    fn = jnp.exp
+    output_values = fn(*input_values)
+
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
+
+
+@pytest.mark.parametrize("shape", [(32, 32), (32, 64)])
+def test_log(shape):
+    test_name = "log"
+
+    x = np.random.normal(size=shape).astype(np.float32)
+    input_values = [x - np.min(x)]
+    fn = jnp.log
+    output_values = fn(*input_values)
+
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
+
+
+# def test_sum():
+#     x = np.random.normal(size=(32, 32)).astype(np.float32)
+#     # y = jnp.sum(x)
+#
+#     print(x)
+#
+#     sum_jit = jit(jnp.sum, inline=True)
+#     xla = jax.xla_computation(sum_jit)(x)
+#     print(xla.as_hlo_text())
+#
+#     with open("sum_as_hlo_text.txt", "w") as f:
+#         f.write(xla.as_hlo_text())
+#     with open("sum_as_hlo_dot_graph.dot", "w") as f:
+#         f.write(xla.as_hlo_dot_graph())
+#
+#     hlo_proto = xla.as_serialized_hlo_module_proto()
+#     with open("sum.hlo_proto", "wb") as f:
+#         f.write(hlo_proto)
+#
+#     with open("sum.hlo_proto", "rb") as f:
+#         add_hlo_proto_data = f.read()
+#         add_hlo_proto = hlo_pb2.HloModuleProto()
+#         add_hlo_proto.ParseFromString(add_hlo_proto_data)
+#
+#     with open("sum_hlo_proto.txt", "w") as f:
+#         f.write(str(add_hlo_proto))
