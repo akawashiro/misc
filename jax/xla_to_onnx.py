4f6bcf43014bd1e522caaacd8370210daf3f8bd3
Akira Kawata <akirakawata1@gmail.com>
2022-03-04 16:18:24 +0900

    WIP

--- jax/xla_to_onnx.py
+++ jax/xla_to_onnx.py
@@ -21,7 +21,7 @@ def translate_dtype(element_type):
         return TensorProto.FLOAT
 
 
-def shape_proto_to_value_info_proto(name, shape_proto):
+def shape_proto_to_value_info_proto(name: str, shape_proto):
     dims = shape_proto.dimensions
     dtype = translate_dtype(shape_proto.element_type)
     return helper.make_tensor_value_info(name, dtype, dims)
@@ -47,8 +47,42 @@ def translate_outputs(tuple_shapes):
     return (names, values)
 
 
+# Instruction -> (name, ValueInfo, Node)
+def t_instruction(instruction):
+    assert(instruction.opcode in ["parameter", "constant", "add", "tuple"])
+    if instruction.opcode == "parameter":
+        name = str(instruction.id)
+        value = shape_proto_to_value_info_proto(str(instruction.id), instruction.shape)
+        return (name, value, None)
+    elif instruction.opcode == "constant":
+        # TODO:
+        return (str(instruction.id), None, None)
+    elif instruction.opcode == "add":
+        inputs = list(map(lambda x:str(x), instruction.operand_ids))
+        node = helper.make_node("Add", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+    elif instruction.opcode == "tuple":
+        # TODO:
+        assert(len(instruction.operand_ids) == 1)
+        inputs = list(map(lambda x:str(x), instruction.operand_ids))
+        node = helper.make_node("Identity", inputs, [str(instruction.id)])
+        return (str(instruction.id), None, node)
+
+
+def t_computation(computation):
+    name_value_nodes = list(map(t_instruction, computation.instructions))
+    input_values = []
+    for n, v, _ in name_value_nodes:
+        if v is not None:
+            input_values.append(n)
+    print(name_value_nodes)
+    print("input_values = ", input_values)
+
+
 # See https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#creating-an-onnx-model-using-helper-functions
 def hlo_proto_to_onnx(hlo_proto, onnx_filename):
+    t_computation(hlo_proto.computations[0])
+
     input_names, input_values = translate_inputs(
         hlo_proto.host_program_shape.parameters
     )
@@ -108,17 +142,30 @@ def test_add():
     assert np.allclose(z, outputs[0])
 
 
-# def test_sum():
-#     x = np.random.normal(size=(32, 32)).astype(np.float32)
-#     y = jnp.sum(x)
-#
-#     print(x)
-#
-#     sum_jit = jit(jnp.sum, inline=True)
-#     xla = jax.xla_computation(sum_jit)(x)
-#     print(xla.as_hlo_text())
-#
-#     with open("sum_as_hlo_text.txt", "w") as f:
-#         f.write(xla.as_hlo_text())
-#     with open("sum_as_hlo_dot_graph.dot", "w") as f:
-#         f.write(xla.as_hlo_dot_graph())
+def test_sum():
+    x = np.random.normal(size=(32, 32)).astype(np.float32)
+    # y = jnp.sum(x)
+
+    print(x)
+
+    sum_jit = jit(jnp.sum, inline=True)
+    xla = jax.xla_computation(sum_jit)(x)
+    print(xla.as_hlo_text())
+
+    with open("sum_as_hlo_text.txt", "w") as f:
+        f.write(xla.as_hlo_text())
+    with open("sum_as_hlo_dot_graph.dot", "w") as f:
+        f.write(xla.as_hlo_dot_graph())
+
+    hlo_proto = xla.as_serialized_hlo_module_proto()
+    with open("sum.hlo_proto", "wb") as f:
+        f.write(hlo_proto)
+
+    with open("sum.hlo_proto", "rb") as f:
+        add_hlo_proto_data = f.read()
+        add_hlo_proto = hlo_pb2.HloModuleProto()
+        add_hlo_proto.ParseFromString(add_hlo_proto_data)
+
+    with open("sum_hlo_proto.txt", "w") as f:
+        f.write(str(add_hlo_proto))
+
