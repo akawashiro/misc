57985830f6223b546df0b0c97bb57f894e1deed4
Akira Kawata <akirakawata1@gmail.com>
2022-03-04 17:40:01 +0900

    WIP

--- jax/xla_to_onnx.py
+++ jax/xla_to_onnx.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import pytest
 
 import jax
 import jax.numpy as jnp
@@ -49,7 +50,7 @@ def translate_outputs(tuple_shapes):
 
 # Instruction -> (name, ValueInfo, Node)
 def t_instruction(instruction):
-    assert(instruction.opcode in ["parameter", "constant", "add", "tuple"])
+    assert instruction.opcode in ["parameter", "constant", "add", "tuple"]
     if instruction.opcode == "parameter":
         name = str(instruction.id)
         value = shape_proto_to_value_info_proto(str(instruction.id), instruction.shape)
@@ -58,40 +59,35 @@ def t_instruction(instruction):
         # TODO:
         return (str(instruction.id), None, None)
     elif instruction.opcode == "add":
-        inputs = list(map(lambda x:str(x), instruction.operand_ids))
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
         node = helper.make_node("Add", inputs, [str(instruction.id)])
         return (str(instruction.id), None, node)
     elif instruction.opcode == "tuple":
         # TODO:
-        assert(len(instruction.operand_ids) == 1)
-        inputs = list(map(lambda x:str(x), instruction.operand_ids))
+        assert len(instruction.operand_ids) == 1
+        inputs = list(map(lambda x: str(x), instruction.operand_ids))
         node = helper.make_node("Identity", inputs, [str(instruction.id)])
         return (str(instruction.id), None, node)
 
 
-def t_computation(computation):
+def t_computation(computation, onnx_filename):
     name_value_nodes = list(map(t_instruction, computation.instructions))
     input_values = []
-    for n, v, _ in name_value_nodes:
+    nodes = []
+    for n, v, node in name_value_nodes:
         if v is not None:
-            input_values.append(n)
+            input_values.append(v)
+        if node is not None:
+            nodes.append(node)
+    output_values = [
+        shape_proto_to_value_info_proto(
+            str(computation.root_id), computation.program_shape.result.tuple_shapes[0]
+        )
+    ]
     print(name_value_nodes)
     print("input_values = ", input_values)
 
-
-# See https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#creating-an-onnx-model-using-helper-functions
-def hlo_proto_to_onnx(hlo_proto, onnx_filename):
-    t_computation(hlo_proto.computations[0])
-
-    input_names, input_values = translate_inputs(
-        hlo_proto.host_program_shape.parameters
-    )
-    output_names, output_values = translate_outputs(
-        hlo_proto.host_program_shape.result.tuple_shapes
-    )
-
-    node_def = helper.make_node("Add", input_names, output_names)
-    graph_def = helper.make_graph([node_def], "test-model", input_values, output_values)
+    graph_def = helper.make_graph(nodes, "test-model", input_values, output_values)
 
     op = onnx.OperatorSetIdProto()
     op.version = 13
@@ -102,44 +98,66 @@ def hlo_proto_to_onnx(hlo_proto, onnx_filename):
     onnx.save(model_def, onnx_filename)
 
 
-def test_add():
-    x = np.random.normal(size=(32, 32)).astype(np.float32)
-    y = np.random.normal(size=(32, 32)).astype(np.float32)
-    input_values = [x, y]
-    z = jnp.add(x, y)
+# See https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#creating-an-onnx-model-using-helper-functions
+def hlo_proto_to_onnx(hlo_proto, onnx_filename):
+    t_computation(hlo_proto.computations[0], onnx_filename)
 
-    # TODO(akawashiro): Use inline=True to remove call
-    add_jit = jit(jnp.add, inline=True)
-    xla = jax.xla_computation(add_jit)(x, y)
-    print(xla.as_hlo_text())
 
-    with open("add_as_hlo_text.txt", "w") as f:
-        f.write(xla.as_hlo_text())
-    with open("add_as_hlo_dot_graph.dot", "w") as f:
-        f.write(xla.as_hlo_dot_graph())
-
-    hlo_proto = xla.as_serialized_hlo_module_proto()
-    with open("add.hlo_proto", "wb") as f:
-        f.write(hlo_proto)
-
-    with open("add.hlo_proto", "rb") as f:
-        add_hlo_proto_data = f.read()
-        add_hlo_proto = hlo_pb2.HloModuleProto()
-        add_hlo_proto.ParseFromString(add_hlo_proto_data)
-
-    with open("add_hlo_proto.txt", "w") as f:
-        f.write(str(add_hlo_proto))
-
-    m = onnx.load("add.onnx")
+def gen_onnx_inputs(onnx_name, input_values):
+    m = onnx.load(onnx_name)
     input_names = list(map(lambda x: x.name, m.graph.input))
     inputs = {}
     assert len(input_names) == len(input_values)
     for n, v in zip(input_names, input_values):
         inputs[n] = v
-    hlo_proto_to_onnx(add_hlo_proto, "add.onnx")
-    ort_sess = ort.InferenceSession("add.onnx")
+    return inputs
+
+
+def translate_and_run(fn, input_values, test_name):
+    onnx_name = test_name + ".onnx"
+
+    # TODO(akawashiro): Use inline=True to remove call
+    fn_jit = jit(fn, inline=True)
+    xla = jax.xla_computation(fn_jit)(*input_values)
+
+    with open(test_name + "_as_hlo_text.txt", "w") as f:
+        f.write(xla.as_hlo_text())
+    with open(test_name + "_as_hlo_dot_graph.dot", "w") as f:
+        f.write(xla.as_hlo_dot_graph())
+
+    hlo_proto = xla.as_serialized_hlo_module_proto()
+    with open(test_name + ".hlo_proto", "wb") as f:
+        f.write(hlo_proto)
+
+    with open(test_name + ".hlo_proto", "rb") as f:
+        hlo_proto_data = f.read()
+        hlo_proto = hlo_pb2.HloModuleProto()
+        hlo_proto.ParseFromString(hlo_proto_data)
+
+    with open(test_name + "_hlo_proto.txt", "w") as f:
+        f.write(str(hlo_proto))
+
+    hlo_proto_to_onnx(hlo_proto, onnx_name)
+
+    inputs = gen_onnx_inputs(onnx_name, input_values)
+    ort_sess = ort.InferenceSession(onnx_name)
     outputs = ort_sess.run(None, inputs)
-    assert np.allclose(z, outputs[0])
+    return outputs
+
+
+@pytest.mark.parametrize("shape", [(32, 32), (32, 64)])
+def test_add(shape):
+    test_name = "add"
+
+    input_values = [
+        np.random.normal(size=shape).astype(np.float32),
+        np.random.normal(size=shape).astype(np.float32),
+    ]
+    fn = jnp.add
+    output_values = fn(*input_values)
+
+    outputs = translate_and_run(fn, input_values, test_name)
+    assert np.allclose(output_values, outputs[0])
 
 
 def test_sum():
@@ -168,4 +186,3 @@ def test_sum():
 
     with open("sum_hlo_proto.txt", "w") as f:
         f.write(str(add_hlo_proto))
-
