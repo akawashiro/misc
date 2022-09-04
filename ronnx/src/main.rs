0d9ddacea6061283304e82bbf840276bc732885d
Akira Kawata <akirakawata1@gmail.com>
2022-09-04 15:51:59 +0900

    Workign

--- ronnx/src/main.rs
+++ ronnx/src/main.rs
@@ -1,40 +1,109 @@
 extern crate protobuf;
 
 use protobuf::{CodedInputStream, Message};
+use std::collections::HashSet;
+use std::fmt;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::BufReader;
+use std::path::Path;
+use std::process::Command;
 
 mod onnx;
 use onnx::ModelProto;
 
+#[derive(PartialEq, Eq, Hash)]
+enum Z3Type {
+    Int,
+}
+
+impl fmt::Display for Z3Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Z3Type::Int => write!(f, "Int"),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash)]
+enum Z3Exp {
+    DecareConst(String, Z3Type),
+    Assert(Box<Z3Exp>),
+    Equal(Box<Z3Exp>, Box<Z3Exp>),
+    CheckSat,
+    GetModel,
+    Variable(String),
+}
+
+impl fmt::Display for Z3Exp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Z3Exp::DecareConst(val, ty) => write!(f, "(declare-const {:} {:})", val, ty),
+            Z3Exp::Assert(exp) => write!(f, "(assert {:})", exp),
+            Z3Exp::Equal(exp1, exp2) => write!(f, "(= {:} {:})", exp1, exp2),
+            Z3Exp::CheckSat => write!(f, "(check-sat)"),
+            Z3Exp::GetModel => write!(f, "(get-model)"),
+            Z3Exp::Variable(var) => write!(f, "{:}", var),
+        }
+    }
+}
+
 fn main() {
-    let file = File::open("squeezenet1.1-7.onnx").expect("fail to open file");
+    assert_eq!(std::env::args().len(), 2);
+    let arg1 = std::env::args().nth(1).unwrap();
+    let onnx_path = Path::new(&arg1);
+    let file = File::open(onnx_path).expect("fail to open file");
     let mut buffered_reader = BufReader::new(file);
     let mut cis = CodedInputStream::from_buf_read(&mut buffered_reader);
 
-    let mut u = ModelProto::new();
-    u.merge_from(&mut cis).expect("fail to merge");
+    let mut model = ModelProto::new();
+    model.merge_from(&mut cis).expect("fail to merge");
 
-    println!("producer name: {}", u.producer_name());
+    let mut decares = HashSet::new();
+    let mut conditions = Vec::new();
 
-    for node in u.graph.node.iter() {
-        println!("name: {:?} op_type: {:?}", node.name, node.op_type);
-        for i in node.input.iter() {
-            println!("input: {:?}", i);
-        }
-        for i in node.output.iter() {
-            println!("output: {:?}", i);
-        }
-        for a in node.attribute.iter() {
-            println!("attribute name: {:?}", a.name);
+    for node in model.graph.node.iter() {
+        if let Some(op_type) = &node.op_type {
+            if op_type == "Relu" {
+                assert_eq!(node.input.len(), 1);
+                assert_eq!(node.input.len(), node.output.len());
+
+                let i = node.input[0].clone() + "_dim_0";
+                let o = node.output[0].clone() + "_dim_0";
+                decares.insert(Z3Exp::DecareConst(i.clone(), Z3Type::Int));
+                decares.insert(Z3Exp::DecareConst(o.clone(), Z3Type::Int));
+                conditions.push(Z3Exp::Assert(Box::new(Z3Exp::Equal(
+                    Box::new(Z3Exp::Variable(i)),
+                    Box::new(Z3Exp::Variable(o)),
+                ))));
+            }
         }
     }
 
-    for value_info in u.graph.value_info.iter() {
-        println!("value_info name: {:?}", value_info.name);
+    let smt_file = arg1 + "_shape_inference.smt";
+    let mut file = File::create(smt_file.clone()).unwrap();
+    let mut contents = String::from("");
+    for d in decares.iter() {
+        contents += &format!("{:}\n", d);
     }
+    for c in conditions.iter() {
+        contents += &format!("{:}\n", c);
+    }
+    contents += &format!("{:}\n", Z3Exp::CheckSat);
+    contents += &format!("{:}\n", Z3Exp::GetModel);
+    file.write_all(contents.as_bytes()).unwrap();
 
-    for input in u.graph.input.iter() {
-        println!("name: {:?}", input);
+    let output = Command::new("z3")
+        .arg("-smt2")
+        .arg(smt_file)
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+        print!("{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+        print!("{}", s);
     }
 }
