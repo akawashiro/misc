5836825eb580acb4df3eb41f2d4b7b868a4fb31a
Akira Kawata <akirakawata1@gmail.com>
2024-09-08 15:03:16 +0900

    WIP

--- elf-space/README.md
+++ elf-space/README.md
@@ -1,8 +1,3 @@
 ```
-nasm -f elf64 ./hello_syscall.asm
-ld -o hello_syscall ./hello_syscall.o
-gcc -o lifegame ./lifegame.c
-gcc -o convert ./convert.c
-./convert
-./hello_syscall_with_parasite
+./run.sh
 ```
--- elf-space/run.sh
+++ elf-space/run.sh
@@ -2,7 +2,8 @@
 
 nasm -f elf64 ./hello_syscall.asm
 ld -o hello_syscall ./hello_syscall.o
-gcc -o lifegame ./lifegame.c
-gcc -o convert ./convert.c
+gcc-11 -o lifegame ./lifegame.c
+gcc-11 -o convert ./convert.c
 ./convert
 ./hello_syscall_with_parasite
+
