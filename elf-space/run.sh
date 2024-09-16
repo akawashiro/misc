bfdbef43ad9bf2ea2d499346cd836b3036982ba5
Akira Kawata <akirakawata1@gmail.com>
2024-09-16 19:23:28 +0900

    WIP

--- elf-space/run.sh
+++ elf-space/run.sh
@@ -5,5 +5,7 @@ ld -o hello ./hello.o
 gcc-11 -o lifegame ./lifegame.c
 gcc-11 -o convert ./convert.c
 ./convert
-./hello_with_parasite
+# ./hello_with_parasite
 
+gcc-11 -o whitespace ./whitespace.c
+./whitespace ./helloworld.ws
