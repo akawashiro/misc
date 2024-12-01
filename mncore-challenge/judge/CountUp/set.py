7bad58485d06f911220ec3f6ccec9ec22b238607
Akira Kawata <akirakawata1@gmail.com>
2024-12-01 19:33:52 +0900

    WIP

--- mncore-challenge/judge/CountUp/set.py
+++ mncore-challenge/judge/CountUp/set.py
@@ -7,8 +7,17 @@ for contents in range(0, 32768, 32 * 8):
             l2bm_addr = (l2bm_addr_offset // (32 * 4)) * 16 + lw
             print(f"d set $lc{l2bm_addr}n{l2b_id // 2}c{l2b_id % 2} 1 s{hex(first)[2:]}_{hex(second)[2:]}")
 
-print(f"mvd/n{16384 // 8} $lc0 $p0@0")
+L2B_DATA_SIZE_LW = 16384 // 8
+for l2bm_addr in range(0, L2B_DATA_SIZE_LW, 64 * 4):
+    print(f"l2bmd $lc{l2bm_addr} $lb{l2bm_addr // 8}")
+
+print("nop/2")
+
+for l2bm_addr in range(0, L2B_DATA_SIZE_LW, 64 * 4):
+    print(f"l2bmd $lb{l2bm_addr // 8} $lc{l2bm_addr + L2B_DATA_SIZE_LW}")
+print("nop/2")
+
+print(f"mvd/n{16384 // 8} $lc{16384 // 8} $p0@0")
 print(f"mvp/n16384 $p0@0 $d0@0")
-for i in range(0, 16384):
-    print(f"d getd $p{i}n0 1")
-    # print(f"d getd $d{i}n0 1")
+# for i in range(0, 16384):
+#     print(f"d getd $p{i}n0 1")
