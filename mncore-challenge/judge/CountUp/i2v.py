affe77a9bcd97951847e5d576107843a2c5dd3bf
Akira Kawata <akirakawata1@gmail.com>
2024-12-05 09:44:47 +0900

    WIP

--- mncore-challenge/judge/CountUp/i2v.py
+++ mncore-challenge/judge/CountUp/i2v.py
@@ -2,27 +2,35 @@ def i2v(l2b: int, l1b: int, mab: int, pe: int, lw: int, addr: int):
     v = 1
     f = 1
 
+    print(f"{f=} at 2_W:1")
     v += lw * f
     f *= 2
 
+    print(f"{f=} at 4_PE:1")
     v += pe * f
     f *= 4
 
+    print(f"{f=} at 2_MAB:1")
     v += (mab % 2) * f
     f *= 2
 
+    print(f"{f=} at 2_L1B:1")
     v += (l1b % 2) * f
     f *= 2
 
+    print(f"{f=} at 8_L2B:1")
     v += l2b * f
     f *= 8
 
+    print(f"{f=} at 4_L1B:2")
     v += (l1b // 2) * f
     f *= 4
 
+    print(f"{f=} at 8_MAB:2")
     v += (mab // 2) * f
     f *= 8
 
+    print(f"{f=} at 4_Addr:1")
     v += addr * f
 
     print(hex(v))
