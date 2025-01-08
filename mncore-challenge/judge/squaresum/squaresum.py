22eef85f4a73151be3183240fe6ace53b925c481
Akira Kawata <akirakawata1@gmail.com>
2025-01-08 19:45:40 +0900

    WIP

--- mncore-challenge/judge/squaresum/squaresum.py
+++
@@ -1,7 +0,0 @@
-for i in range(0, 256, 8):
-    if i == 0:
-        print(f"dvmulu $lm{i}v $lm{i}v $nowrite")
-    else:
-        print(f"dvfmau $lm{i}v $lm{i}v $mauf $nowrite")
-    print(f"dvfmad $lm{i}v $lm{i}v $mauf $nowrite")
-print(f"lpassa $mauf $lr0v")
