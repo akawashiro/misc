f01fdb5f887e6e4374a2ead6fb5872ee8aeb3fac
Akira Kawata <akirakawata1@gmail.com>
2025-02-08 17:48:40 +0900

    WIP

--- cpu/cpu/cpu.sv
+++ cpu/cpu/cpu.sv
@@ -114,6 +114,17 @@ module sign_extend (
     assign imm_ext = {{20{imm[11]}}, imm};
 endmodule
 
+module control_unit (
+    input logic [6:0] opcode,
+    input logic [2:0] funct3,
+    input logic [6:0] funct7,
+    output logic [2:0] alu_op
+);
+    always_comb begin
+        alu_op = ADD;
+    end
+endmodule
+
 module cpu (
     input logic clk,
     input logic reset,
