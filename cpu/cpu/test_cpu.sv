8ad383edc86f9a929558e12134eb2682a7d4f8b6
Akira Kawata <akirakawata1@gmail.com>
2025-02-11 14:05:24 +0900

    WIP

--- cpu/cpu/test_cpu.sv
+++ cpu/cpu/test_cpu.sv
@@ -731,3 +731,38 @@ module test_cpu_xori;
         assert(register_check[8] == 0) else $error("register_check[8] = %d", register_check[8]);
     end
 endmodule
+
+logic [31:0] ori_x8_x7_1 = 32'h0013e413;
+module test_cpu_ori;
+    logic clk;
+    logic reset;
+    logic [31:0] initial_instructions [31:0];
+    logic [31:0] initial_register_values [31:0];
+    wire [31:0] register_check [0:31];
+
+    assign initial_instructions[0] = ori_x8_x7_1;
+    assign initial_register_values[7] = 1;
+
+    cpu cpu_0 (
+        .clk(clk),
+        .reset(reset),
+        .initial_instructions(initial_instructions),
+        .initial_register_values(initial_register_values),
+        .register_check(register_check)
+    );
+
+    initial begin
+        clk = 0;
+        reset = 0;
+        #10
+        clk = 1;
+        reset = 1;
+        #10
+        reset = 0;
+        clk = 0;
+        #10
+        clk = 1;
+        #10
+        assert(register_check[8] == 1) else $error("register_check[8] = %d", register_check[8]);
+    end
+endmodule
