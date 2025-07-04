a9ac109c81a9e0f4707359236abe9c29dd7ef4e8
Akira Kawata <akirakawata1@gmail.com>
2025-07-04 23:41:01 +0900

    WIP

---
+++ bandwidth/common.h
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+
+constexpr uint64_t DATA_SIZE = 128 * (1 << 20);
+constexpr uint64_t CHECKSUM_SIZE = 128;
+constexpr uint64_t CONTEXT_SIZE = DATA_SIZE - CHECKSUM_SIZE;
+static_assert(DATA_SIZE > CHECKSUM_SIZE,
+              "DATA_SIZE must be larger than CHECKSUM_SIZE");
+
+std::vector<uint8_t> calcChecksum(const std::vector<uint8_t> &data) {
+  std::vector<uint8_t> checksum(CHECKSUM_SIZE, 0);
+  for (size_t i = 0; i < CONTEXT_SIZE; ++i) {
+    checksum[i % CHECKSUM_SIZE] ^= data[i];
+  }
+  return checksum;
+}
+
+std::vector<uint8_t> generateDataToSend() {
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+  std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+  std::vector<uint8_t> data(DATA_SIZE, 0);
+
+  size_t i = 0;
+  for (i = 0; i + 8 < CONTEXT_SIZE; i += 8) {
+    uint64_t *d = reinterpret_cast<uint64_t *>(&data[i]);
+    *d = dist(engine);
+  }
+  for (; i < CONTEXT_SIZE; ++i) {
+    data[i] = static_cast<uint8_t>(dist(engine) & 0xFF);
+  }
+  const std::vector<uint8_t> checksum = calcChecksum(data);
+  for (size_t j = 0; j < CHECKSUM_SIZE; ++j) {
+    data[CONTEXT_SIZE + j] = checksum[j];
+  }
+
+  return data;
+}
+
+bool verifyDataReceived(const std::vector<uint8_t> &data) {
+  if (data.size() != DATA_SIZE) {
+    LOG(ERROR) << "Data size mismatch: expected " << DATA_SIZE << ", got "
+               << data.size();
+    return false;
+  }
+
+  std::vector<uint8_t> checksum = calcChecksum(data);
+  for (size_t i = 0; i < CHECKSUM_SIZE; ++i) {
+    if (data[CONTEXT_SIZE + i] != checksum[i]) {
+      LOG(ERROR) << "Checksum mismatch at index " << i << ": expected "
+                 << static_cast<int>(checksum[i]) << ", got "
+                 << static_cast<int>(data[CONTEXT_SIZE + i]);
+      return false;
+    }
+  }
+  return true;
+}
+
+constexpr int NUM_WARMUPS = 3;
+constexpr int NUM_ITERATIONS = 10;
+
+double calculateBandwidth(std::vector<double> durations) {
+  CHECK(durations.size() == NUM_ITERATIONS);
+  std::sort(durations.begin(), durations.end());
+  std::vector<double> filtered_durations(durations.begin() + 1,
+                                         durations.end() - 1);
+
+  double average_duration = std::accumulate(filtered_durations.begin(),
+                                            filtered_durations.end(), 0.0) /
+                            filtered_durations.size();
+  double bandwidth = DATA_SIZE / average_duration;
+  return bandwidth;
+}
+
+
