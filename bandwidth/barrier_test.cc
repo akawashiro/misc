2d93a8fed2049d69cc6dd84985d2b2d98ac9a463
Akira Kawata <akirakawata1@gmail.com>
2025-07-07 21:32:45 +0900

    WIP

--- bandwidth/barrier_test.cc
+++ bandwidth/barrier_test.cc
@@ -1,9 +1,14 @@
+#include <sys/wait.h>
+
+#include <optional>
+
+#include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "absl/log/check.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
-#include <sys/wait.h>
 
 class SenseReversingBarrier {
 public:
@@ -14,18 +19,23 @@ private:
   const std::string id_;
 };
 
+ABSL_FLAG(std::optional<int>, vlog, std::nullopt,
+          "Show VLOG messages lower than this level.");
+
 int main(int argc, char *argv[]) {
   absl::SetProgramUsageMessage("Sense Reversing Barrier Test");
   absl::ParseCommandLine(argc, argv);
+  std::optional<int> vlog = absl::GetFlag(FLAGS_vlog);
+  if (vlog.has_value()) {
+    int v = *vlog;
+    absl::SetGlobalVLogLevel(v);
+  }
+  absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
+  absl::InitializeLog();
 
-  // Create a barrier with 4 participants
   SenseReversingBarrier barrier(4, "test_barrier");
 
-  // Simulate some work
   LOG(INFO) << "Starting barrier test";
-
-  // Wait for all participants to reach the barrier
-  // (In a real implementation, you would use synchronization primitives here)
 
   LOG(INFO) << "Barrier test completed successfully.";
 
