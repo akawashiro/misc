98e682d366e600f7abdbf00ee3118aedbc254160
Akira Kawata <akirakawata1@gmail.com>
2025-07-08 21:24:52 +0900

    WIP

---
+++ bandwidth/barrier.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <semaphore.h>
+
+class SenseReversingBarrier {
+public:
+  SenseReversingBarrier(int n, const std::string &id);
+  ~SenseReversingBarrier();
+  void Wait();
+
+private:
+  struct ShmData {
+    uint64_t count_{0};
+    bool shared_sense_{false};
+    uint64_t n_users_{0};
+  };
+
+  sem_t *shm_sem_;
+  int shm_fd_;
+  ShmData *shm_data_;
+  bool sense_ = true;
+
+  const uint64_t n_;
+  const std::string init_sem_id_;
+  const std::string shm_sem_id_;
+  const std::string shm_id_;
+};
--- bandwidth/barrier_test.cc
+++ bandwidth/barrier_test.cc
@@ -1,6 +1,3 @@
-#include <fcntl.h>
-#include <semaphore.h>
-#include <sys/mman.h>
 #include <sys/wait.h>
 
 #include <optional>
@@ -15,158 +12,7 @@
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
 
-class SenseReversingBarrier {
-public:
-  SenseReversingBarrier(int n, const std::string &id)
-      : n_(n), init_sem_id_(id + "_init_sem"), shm_sem_id_(id + "_shm_sem"),
-        shm_id_(id + "_shm") {
-    shm_sem_ = sem_open(shm_sem_id_.c_str(), O_CREAT, 0644, 1);
-    CHECK(shm_sem_ != SEM_FAILED) << "Failed to create semaphore with id '"
-                                  << shm_sem_id_ << "': " << strerror(errno);
-
-    // Critical section to ensure all processes hold initialized shared memory.
-    sem_wait(shm_sem_);
-    shm_fd_ = shm_open(shm_id_.c_str(), O_CREAT | O_RDWR | O_EXCL, 0644);
-    if (shm_fd_ >= 0) {
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - Created shared memory with id '" << shm_id_ << "'";
-      CHECK(ftruncate(shm_fd_, sizeof(ShmData)) == 0)
-          << "Failed to set size of shared memory with id '" << shm_id_
-          << "': " << strerror(errno);
-      shm_data_ = static_cast<ShmData *>(mmap(nullptr, sizeof(ShmData),
-                                              PROT_READ | PROT_WRITE,
-                                              MAP_SHARED, shm_fd_, 0));
-      CHECK(shm_data_ != MAP_FAILED) << "Failed to map shared memory with id '"
-                                     << shm_id_ << "': " << strerror(errno);
-
-      shm_data_->count_ = 0;
-      shm_data_->shared_sense_ = false;
-      shm_data_->n_users_ = 0;
-    } else if (shm_fd_ < 0) {
-      if (errno == EEXIST) {
-        VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-                << " - Shared memory with id '" << shm_id_
-                << "' already exists, opening it instead.";
-        shm_fd_ = shm_open(shm_id_.c_str(), O_RDWR, 0644);
-        CHECK(shm_fd_ >= 0) << "Failed to open existing shared memory with id '"
-                            << shm_id_ << "': " << strerror(errno);
-        shm_data_ = static_cast<ShmData *>(mmap(nullptr, sizeof(ShmData),
-                                                PROT_READ | PROT_WRITE,
-                                                MAP_SHARED, shm_fd_, 0));
-        CHECK(shm_data_ != MAP_FAILED)
-            << "Failed to map existing shared memory with id '" << shm_id_
-            << "': " << strerror(errno);
-      } else {
-        CHECK(false) << "Failed to create shared memory with id '" << shm_id_
-                     << "': " << strerror(errno);
-      }
-    }
-    sem_post(shm_sem_);
-    // Critical section ends here.
-
-    sem_wait(shm_sem_);
-    shm_data_->n_users_++;
-    sem_post(shm_sem_);
-
-    VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-            << " - SenseReversingBarrier initialized with id '" << shm_id_
-            << "' for " << n_ << " users. Waiting for all users to join.";
-    while (true) {
-      sem_wait(shm_sem_);
-      bool all_users_joined = shm_data_->n_users_ >= n_;
-      sem_post(shm_sem_);
-      if (all_users_joined) {
-        break;
-      }
-      std::this_thread::yield();
-    }
-    VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-            << " - All users have joined the barrier with id '" << shm_id_
-            << "'. Proceeding.";
-  }
-
-  void Wait() {
-    bool last_user = false;
-    sem_wait(shm_sem_);
-    shm_data_->count_++;
-    if (shm_data_->count_ == n_) {
-      last_user = true;
-      shm_data_->shared_sense_ = !shm_data_->shared_sense_;
-      shm_data_->count_ = 0;
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - All users reached the barrier with id '" << shm_id_
-              << "'. Sense reversed to " << shm_data_->shared_sense_;
-    }
-    sem_post(shm_sem_);
-
-    if (!last_user) {
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - Waiting for other users to reach the barrier with id '"
-              << shm_id_ << "'.";
-      while (true) {
-        sem_wait(shm_sem_);
-        bool reversed = shm_data_->shared_sense_ != sense_;
-        sem_post(shm_sem_);
-        if (!reversed) {
-          break;
-        }
-      }
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - All users reached the barrier with id '" << shm_id_;
-    }
-
-    sense_ = !sense_;
-  }
-
-  ~SenseReversingBarrier() {
-    sem_wait(shm_sem_);
-    uint64_t remaining_users = shm_data_->n_users_;
-    shm_data_->n_users_--;
-    sem_post(shm_sem_);
-    if (remaining_users == 1) {
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - Last user of shared memory with id '" << shm_id_
-              << "' is exiting. Unlinking shared memory.";
-      if (shm_sem_) {
-        sem_close(shm_sem_);
-        sem_unlink(shm_sem_id_.c_str());
-      }
-      if (shm_fd_ >= 0) {
-        munmap(shm_data_, sizeof(ShmData));
-        close(shm_fd_);
-        shm_unlink(shm_id_.c_str());
-      }
-    } else {
-      VLOG(1) << "PID: " << getpid() << " TID: " << pthread_self()
-              << " - Not the last user of shared memory with id '" << shm_id_
-              << " " << remaining_users << " users remaining.";
-      if (shm_sem_) {
-        sem_close(shm_sem_);
-      }
-      if (shm_fd_ >= 0) {
-        munmap(shm_data_, sizeof(ShmData));
-        close(shm_fd_);
-      }
-    }
-  }
-
-private:
-  struct ShmData {
-    uint64_t count_{0};
-    bool shared_sense_{false};
-    uint64_t n_users_{0};
-  };
-
-  sem_t *shm_sem_;
-  int shm_fd_;
-  ShmData *shm_data_;
-  bool sense_ = true;
-
-  const uint64_t n_;
-  const std::string init_sem_id_;
-  const std::string shm_sem_id_;
-  const std::string shm_id_;
-};
+#include "barrier.h"
 
 namespace {
 void TestConstructor() {
@@ -184,7 +30,7 @@ void TestConstructor() {
 }
 
 void TestWait() {
-  constexpr int ITERATIONS = 1000;
+  constexpr int ITERATIONS = 20;
   constexpr double MAX_WAIT_MS = 100.0;
   int pid = fork();
   CHECK(pid >= 0) << "Fork failed: " << strerror(errno);
