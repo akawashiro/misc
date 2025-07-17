e58ca74bfc008e372bcadd0b468a7daf20e29f03
Akira Kawata <akirakawata1@gmail.com>
2025-07-17 22:12:22 +0900

    WIP

--- bandwidth/build.sh
+++ bandwidth/build.sh
@@ -5,8 +5,9 @@ set -eux -o pipefail
 git submodule update --init --recursive
 cmake -S . \
     -B build \
-    -D CMAKE_CXX_COMPILER=clang++-18 \
+    -D CMAKE_CXX_COMPILER=clang++ \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo \
     -D CMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
     -G Ninja
 cmake --build build
